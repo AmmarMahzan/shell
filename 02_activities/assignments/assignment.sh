0c93bd96e1da533b72881be5bbea47028dbc1a41
AmmarMahzan <ammarmahzan@gmail.com>
2025-04-21 19:13:51 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -22,38 +22,34 @@ unzip -q rawdata.zip
 ###########################################
 # Complete assignment here
 
-# 1. Create a directory named data and move rawdata into data/raw
-mkdir -p data/raw
-mv rawdata data/raw
+# 1. Create a directory named data and move contents of rawdata into it
+dmkdir -p data/raw
+# Move all rawdata files into data/raw and remove the now-empty rawdata folder
+mv rawdata/* data/raw/
+rmdir rawdata
 
 # 2. List the contents of the ./data/raw directory
 echo "Contents of data/raw:"
 ls data/raw
 
-# 3. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir -p data/processed/{server_logs,user_logs,event_logs}
+# 3. In ./data/processed, create the following directories:
+mkdir -p data/processed/server_logs data/processed/user_logs data/processed/event_logs
 
-# 4. Copy all server log files ("server" in name, .log extension) from raw to processed/server_logs
-find data/raw -maxdepth 1 -type f -name "*server*.log" -exec cp {} data/processed/server_logs/ \;
+# 4. Copy all server log files from ./data/raw to ./data/processed/server_logs
+find data/raw -type f -name "*server*.log" -exec cp {} data/processed/server_logs/ \;
 
-# 5. Copy all user log files to processed/user_logs
-find data/raw -maxdepth 1 -type f -name "*user*.log" -exec cp {} data/processed/user_logs/ \;
+# 5. Copy all user log files to ./data/processed/user_logs
+find data/raw -type f -name "*user*.log" -exec cp {} data/processed/user_logs/ \;
 
-# 6. Copy all event log files to processed/event_logs
-find data/raw -maxdepth 1 -type f -name "*event*.log" -exec cp {} data/processed/event_logs/ \;
+# 6. Copy all event log files to ./data/processed/event_logs
+find data/raw -type f -name "*event*.log" -exec cp {} data/processed/event_logs/ \;
 
-# 7. Remove files containing "ipaddr" from raw and processed/user_logs
-find data/raw -type f -name "*ipaddr*" -delete
+# 7. Remove files containing "ipaddr" for user privacy
+find data/raw               -type f -name "*ipaddr*" -delete
 find data/processed/user_logs -type f -name "*ipaddr*" -delete
 
-# 8. Create inventory of all files in subfolders of processed
+# 8. Create a file named ./data/inventory.txt listing all files in data/processed
 find data/processed -type f > data/inventory.txt
-
-###########################################
-
-echo "Project setup is complete!"
-# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
 
 ###########################################
 
